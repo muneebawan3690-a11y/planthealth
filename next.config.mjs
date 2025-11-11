3bf920d0239a100c7f7af8348388ef253d0bf93e
Muneeb Awan <muneebawan3690@gmail.com>
2025-11-11 13:18:00 +0500

    Update next.config.mjs for remotePatterns and remove eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
+    // Allows production builds even with type errors
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
-    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+    unoptimized: true, // Disable Next.js image optimization
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**", // Allow all paths under this domain
+      },
+    ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
